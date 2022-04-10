e8f3bd9bf810656e03edc8b550de20c42151d82f
Khaled Nassar <kmhnassar@gmail.com>
2022-04-10 13:31:13 +0200

    Final fixes for the AP saga - closes #22

--- src/setup_apmode.sh
+++ src/setup_apmode.sh
@@ -25,4 +25,22 @@ echo -e "listener 1883 0.0.0.0\nallow_anonymous true\n" >> /etc/mosquitto/mosqui
 
 rfkill unblock wifi
 
-printf "ssid=cloudcutterflash\nchannel=1\nlogger_stdout_level=4\nhw_mode=g\ninterface=$WLAN" | hostapd /dev/stdin -P $(pwd)/hostapd.pid -B
+# Set up hostapd with
+# 1. 802.11n in 2.4GHz (hw_mode=g) - some devices scan for it
+# 2. WPA2-PSK - some devices do not connect otherwise
+# 3. Enforced WPA2 - same as above
+hostapd /dev/stdin -P $(pwd)/hostapd.pid -B <<- EOF
+ssid=cloudcutterflash
+channel=1
+logger_stdout_level=4
+hw_mode=g
+ieee80211n=1
+wmm_enabled=1
+interface=$WLAN
+auth_algs=1
+wpa=2
+wpa_key_mgmt=WPA-PSK
+wpa_passphrase=abcdabcd
+rsn_pairwise=CCMP
+EOF
+
