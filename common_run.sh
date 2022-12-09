5076d368a67886e5c4099dfa344dce7f8d70fc34
Cossid <cossid@cossid.com>
2022-12-09 12:18:15 -0600

    common_run - If firmware is selected, and the filename indicates a chip different that the selected profile, force the user to confim they want to proceed.  If there is no chip name in the filename, it will continue with no warning.

--- common_run.sh
+++ common_run.sh
@@ -13,6 +13,24 @@ fi
 PROFILE=$(cat profile.txt)
 rm -f profile.txt
 
+# Check if chip in profile mismatches a chip in the firmware name if there is one present.
+if ! [ -z "${FIRMWARE}" ]; then
+  LOCAL_PROFILE=$(echo "${PROFILE}" | sed 's/\/work\///g')
+  CHIP=$(cat "${LOCAL_PROFILE}" | grep -o '"chip": "[^"]*' | grep -o '[^"]*$')
+  if grep -q "BK7231" <<< "${FIRMWARE}"; then
+	if ! grep -q "${CHIP}" <<< "${FIRMWARE}"; then
+	  echo "WARNING: Flashing a firmware for the wrong chip may soft-brick your device requiring a serial flash to recover."
+	  echo "You have selected a profile for chip type ${CHIP} but the selected firmware filename (${FIRMWARE}) indicates it may be for another chip."
+	  read -p "Are you sure want to proceed?  Type 'PROCEED' (case-sensitive) to continue or 'exit' to stop (exit/PROCEED): " doublecheck
+	  case $doublecheck in 
+	    "PROCEED" ) echo "Proceding with selected profile for chip ${CHIP} and firmware ${FIRMWARE}";
+		  break;;
+	    * ) exit;;
+      esac
+	fi
+  fi
+fi
+
 # Connect to Tuya device's WiFi
 echo "==> Toggle Tuya device's power off and on again 6 times, with ~1 sec pauses in between, to enable AP mode. Repeat if your device's SSID doesn't show up within ~30 seconds. For smart plugs long press the reset button on the device for about 5 seconds. See https://support.tuya.com/en/help/_detail/K9hut3w10nby8 for more information."
 wifi_connect
