8c26a13a3d3ce55299368c58c0632b7aa2c095f0
Cossid <cossid@cossid.com>
2022-12-29 12:38:50 -0600

    cloudcutter - Remove sectioning for logging, as it appears to be causing issues.

--- tuya-cloudcutter.sh
+++ tuya-cloudcutter.sh
@@ -94,41 +94,39 @@ fi
 
 source common_run.sh
 
-{
-	if [ $METHOD_DETACH ]; then
-		# Cutting device from cloud, allowing local-tuya access still
-		echo "Cutting device off from cloud.."
-		echo "==> Wait for 20-30 seconds for the device to connect to 'cloudcutterflash'. This script will then show the activation requests sent by the device, and tell you whether local activation was successful."
-		nmcli device set ${WIFI_ADAPTER} managed no; service NetworkManager stop;
-		trap "service NetworkManager start; nmcli device set ${WIFI_ADAPTER} managed yes" EXIT  # Set WiFi adapter back to managed when the script exits
-		INNER_SCRIPT=$(xargs -0 <<- EOF
-			# This janky looking string substitution is because of double evaluation.
-			# Once in the parent shell script, and once in this heredoc used as a shell script.
-			# First evaluate the value from the parent shell script while escaping ' chars
-			# with this janky substitutions so that it doesn't break this heredoc script.
-			SSID='${SSID/\'/\'\"\'\"\'}'
-			SSID_PASS='${SSID_PASS/\'/\'\"\'\"\'}'
-			bash /src/setup_apmode.sh ${WIFI_ADAPTER}
-			pipenv run python3 -m cloudcutter configure_local_device --ssid "\${SSID}" --password "\${SSID_PASS}" "${PROFILE}" "/work/device-profiles/schema" "${CONFIG_DIR}"
-		EOF
-		)
-		run_in_docker bash -c "$INNER_SCRIPT"
-		if [ ! $? -eq 0 ]; then
-			echo "Oh no, something went wrong with detaching from the cloud! Try again I guess.."
-			exit 1
-		fi
+if [ $METHOD_DETACH ]; then
+	# Cutting device from cloud, allowing local-tuya access still
+	echo "Cutting device off from cloud.."
+	echo "==> Wait for 20-30 seconds for the device to connect to 'cloudcutterflash'. This script will then show the activation requests sent by the device, and tell you whether local activation was successful."
+	nmcli device set ${WIFI_ADAPTER} managed no; service NetworkManager stop;
+	trap "service NetworkManager start; nmcli device set ${WIFI_ADAPTER} managed yes" EXIT  # Set WiFi adapter back to managed when the script exits
+	INNER_SCRIPT=$(xargs -0 <<- EOF
+		# This janky looking string substitution is because of double evaluation.
+		# Once in the parent shell script, and once in this heredoc used as a shell script.
+		# First evaluate the value from the parent shell script while escaping ' chars
+		# with this janky substitutions so that it doesn't break this heredoc script.
+		SSID='${SSID/\'/\'\"\'\"\'}'
+		SSID_PASS='${SSID_PASS/\'/\'\"\'\"\'}'
+		bash /src/setup_apmode.sh ${WIFI_ADAPTER}
+		pipenv run python3 -m cloudcutter configure_local_device --ssid "\${SSID}" --password "\${SSID_PASS}" "${PROFILE}" "/work/device-profiles/schema" "${CONFIG_DIR}"
+	EOF
+	)
+	run_in_docker bash -c "$INNER_SCRIPT"
+	if [ ! $? -eq 0 ]; then
+		echo "Oh no, something went wrong with detaching from the cloud! Try again I guess.."
+		exit 1
 	fi
+fi
 
-	if [ $METHOD_FLASH ]; then
-		# Flash custom firmware to device
-		echo "Flashing custom firmware .."
-		echo "==> Wait for 20-30 seconds for the device to connect to 'cloudcutterflash'. This script will then show the firmware upgrade requests sent by the device."
-		nmcli device set "${WIFI_ADAPTER}" managed no
-		trap "nmcli device set ${WIFI_ADAPTER} managed yes" EXIT  # Set WiFi adapter back to managed when the script exits
-		run_in_docker bash -c "bash /src/setup_apmode.sh ${WIFI_ADAPTER} && pipenv run python3 -m cloudcutter update_firmware \"${PROFILE}\" \"/work/device-profiles/schema\" \"${CONFIG_DIR}\" \"/work/custom-firmware/\" \"${FIRMWARE}\""
-		if [ ! $? -eq 0 ]; then
-			echo "Oh no, something went wrong with updating firmware! Try again I guess.."
-			exit 1
-		fi
+if [ $METHOD_FLASH ]; then
+	# Flash custom firmware to device
+	echo "Flashing custom firmware .."
+	echo "==> Wait for 20-30 seconds for the device to connect to 'cloudcutterflash'. This script will then show the firmware upgrade requests sent by the device."
+	nmcli device set "${WIFI_ADAPTER}" managed no
+	trap "nmcli device set ${WIFI_ADAPTER} managed yes" EXIT  # Set WiFi adapter back to managed when the script exits
+	run_in_docker bash -c "bash /src/setup_apmode.sh ${WIFI_ADAPTER} && pipenv run python3 -m cloudcutter update_firmware \"${PROFILE}\" \"/work/device-profiles/schema\" \"${CONFIG_DIR}\" \"/work/custom-firmware/\" \"${FIRMWARE}\""
+	if [ ! $? -eq 0 ]; then
+		echo "Oh no, something went wrong with updating firmware! Try again I guess.."
+		exit 1
 	fi
-} #2>&1 | tee -a ${LOGFILE}
+fi
